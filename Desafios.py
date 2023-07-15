3fbacbabdefeb9b58a97af6e47adf1235da2bfb0
Ana R. Carvalho <137975286+AnaRaquelCarvalho@users.noreply.github.com>
2023-07-15 15:13:04 -0300

    Novos

---
+++ Desafios.py
@@ -0,0 +1,13 @@
+print('Olá, Mundo!')
+print(7+4)
+print('7'+'4')
+
+nome = 'Guanabara'
+idade = 25
+peso = 78,8
+print(nome, idade,peso)
+
+nome = input('Qual é o seu nome? ')
+idade = input('Quantos anos voce tem? ')
+peso = input('Informe o seu peso:')
+print(nome, idade, peso)
---
+++ desafio3.py
@@ -0,0 +1,7 @@
+print('====== Desafio 1 ======')
+n1 = float(input('Digite um número: '))
+n2 = float(input('Digite um segundo número: '))
+n3 = float(input('Digite um terceiro número: '))
+s = n1 + n2 + n3
+print('A soma é: ',s)
+
