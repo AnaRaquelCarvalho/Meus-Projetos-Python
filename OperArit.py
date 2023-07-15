13b5437eca308d0c62b38b24e68bfe320a0f5ed0
Ana R. Carvalho <137975286+AnaRaquelCarvalho@users.noreply.github.com>
2023-07-15 15:18:14 -0300

    Operadores Aritméticos
    Lista de Operadores Aritméticos

---
+++ OperArit.py
@@ -0,0 +1,29 @@
+print('==== Operadores Aritméticos ====')
+
+# Adição
+print(5+3)
+
+# Subtração
+print(5-3)
+
+# Multiplicação
+print(5*3)
+
+# Divisão
+print(5/3)
+
+# Potência ou Exponênciação (Normal ou c/ Pow)
+print(5**3)
+print(pow(5,3))
+
+# Divisão Inteira
+print(5//3)
+
+# Resto da Divisão ou Módulo
+print(5 % 3)
+
+# Raiz Quadrada
+print(81**(1/2))
+
+# Raiz Cúbica
+print(127**(1/3))
