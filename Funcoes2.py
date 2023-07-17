54442a0cd87a67f4420684d10b43e48b5c04f43e
Ana R. Carvalho <137975286+AnaRaquelCarvalho@users.noreply.github.com>
2023-07-17 16:31:16 -0300

    Funções Python 2
    Algumas funcionalidades com o Python

---
+++ Funcoes2.py
@@ -0,0 +1,14 @@
+print('='*14, 'Exercicios 9', '='*14)
+# Exemplos de Prints de Adição
+
+# 1º Exemplo
+n1 = int(input('Informe um número: '))
+n2 = int(input('Informe um segundo número: '))
+s = n1 + n2
+print('A soma é igual a {}'.format(s))
+
+# 2º Exemplo
+n1 = int(input('Informe um valor: '))
+n2 = int(input('Informe outro valor: '))
+print('A soma vale {}'.format(n1 + n2))
+
