9b29e8a58bda42465f07b8fa2a8c15c300c735c8
Ana R. Carvalho <137975286+AnaRaquelCarvalho@users.noreply.github.com>
2023-07-17 22:19:43 -0300

    Antecessor e Sucessor de um número informado
    Informe um número e mostre seu antecessor e sucessor

---
+++ Ant&suc.py
@@ -0,0 +1,5 @@
+print('='*10,'Desafio (Antecessor e Sucessor)','='*10)
+n1 = int(input('Informe um número: '))
+print(n1 - 1,',',n1,',',n1 + 1,'\n')
+print(' Antecessor: {}\n Número: {}\n Sucessor: {}\n'.format(n1 - 1, n1, n1 + 1))
+print('-'*16)
