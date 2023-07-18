8c8ad1e78e9b97fa0c049646310a4800bf1cadf5
Ana R. Carvalho <137975286+AnaRaquelCarvalho@users.noreply.github.com>
2023-07-17 23:41:24 -0300

    Dobro, Triplo e Raiz Quadrada de um Número

---
+++ Desafio4.py
@@ -0,0 +1,7 @@
+print('='* 16,'Desafio 3','='*16)
+
+# O dobro, o O Triplo e a Raiz Quadrada de um número informado
+n1 = int(input('Informe um número: '))
+print('O Numero é : {}\nO dobro de {} é {}\nO triplo de {} é {}\nA Raiz Quadrada de {} é {}'
+      .format(n1, n1, n1 * 2,n1, n1 * 3, n1, n1**(1/2)))
+print('-'*44)
