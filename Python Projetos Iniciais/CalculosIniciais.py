68400e145572cb06cb7e8a7861cb68cbb1cdef9d
Ana R. Carvalho <137975286+AnaRaquelCarvalho@users.noreply.github.com>
2023-07-20 19:00:33 -0300

    Delete Python Projetos Iniciais directory

--- Python Projetos Iniciais/CalculosIniciais.py
+++
@@ -1,5 +0,0 @@
-print('=== Calculando o dobro, triplo e a raiza quadrada de um número ===\n')
-
-n1 = int(input('Informe um número: '))
-
-print(' Número: {}.\n O dobro de {} é {}.\n O triplo de {} é {}.\n A raiz quadrada de {} é {:.0f}.'.format(n1,n1,(n1 * 2),n1,(n1 * 3),n1,n1**(1/2)))
