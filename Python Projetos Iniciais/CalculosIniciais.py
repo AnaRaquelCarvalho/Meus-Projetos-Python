585890aef0873f7d825691d2d479f72259b7236f
Ana R. Carvalho <137975286+AnaRaquelCarvalho@users.noreply.github.com>
2023-07-20 17:26:32 -0300

    Calculando
    O dobro, o triplo e a Raiz Quadrada de um número

---
+++ Python Projetos Iniciais/CalculosIniciais.py
@@ -0,0 +1,5 @@
+print('=== Calculando o dobro, triplo e a raiza quadrada de um número ===\n')
+
+n1 = int(input('Informe um número: '))
+
+print(' Número: {}.\n O dobro de {} é {}.\n O triplo de {} é {}.\n A raiz quadrada de {} é {:.0f}.'.format(n1,n1,(n1 * 2),n1,(n1 * 3),n1,n1**(1/2)))
