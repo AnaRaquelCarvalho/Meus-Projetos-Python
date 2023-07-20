03e293b20d8334af684d9db7a67c60e2baa56f69
Ana R. Carvalho <137975286+AnaRaquelCarvalho@users.noreply.github.com>
2023-07-20 17:03:48 -0300

    Desafio
    Reformulação

--- Projetos Python/Ant&suc.py
+++ Projetos Python/Ant&suc.py
@@ -1,4 +1,4 @@
-print('='*10,'Desafio (Antecessor e Sucessor)','='*10)
+print('=== Desafio (Antecessor e Sucessor) ===')
 n1 = int(input('Informe um número: '))
 print(n1 - 1,',',n1,',',n1 + 1,'\n')
 print(' Antecessor: {}\n Número: {}\n Sucessor: {}\n'.format(n1 - 1, n1, n1 + 1))
